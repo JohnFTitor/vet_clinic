794672924bec14a7b9caa1828fd8f0ae6a9fb7e8
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-03-21 16:53:50 -0500

    Add data to table

--- data.sql
+++ data.sql
@@ -1,5 +1,10 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals
+VALUES(1, 'Agumon', '3-feb-2020', 0, true, 10.23);
+INSERT INTO animals
+VALUES(2, 'Gabumon', '15-nov-2018', 2, true, 8);
+INSERT INTO animals
+VALUES(3, 'Pikachu', '7-jan-2021', 1, false, 15.04);
+INSERT INTO animals
+VALUES(4, 'Devimon', '12-may-2017', 5, true, 11);
