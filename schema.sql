87722e65ad5c5125fba0a678acf45815b5408e84
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-03-21 16:36:58 -0500

    Create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE ANIMALS (
+    id INT,
+    name VARCHAR,
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL  
 );
