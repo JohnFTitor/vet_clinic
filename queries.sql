63a2cb2b25303f8ab441be8589623e7a27120feb
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-03-22 15:19:32 -0500

    Make queries to answer required questions

--- queries.sql
+++ queries.sql
@@ -8,3 +8,22 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+/* Logical Queries */
+
+SELECT COUNT(*) as quantity_of_animals FROM animals;
+
+SELECT COUNT(*) as quantity_of_animals_who_has_not_escaped FROM animals
+WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) as average_weight FROM animals;
+
+SELECT neutered, SUM(escape_attempts) as escape_Attempts FROM animals
+GROUP BY neutered;
+
+SELECT species, MIN(weight_kg) as min_weight, MAX(weight_kg) as max_weight FROM animals
+GROUP BY species;
+
+SELECT species, AVG(escape_Attempts) as avergage_escape_attempts FROM animals
+WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 1990 AND 2000
+GROUP BY species;
